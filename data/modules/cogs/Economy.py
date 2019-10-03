879e97b18ad231307a6349df512b775416f0eb57
BuildTools <unconfigured@null.spigotmc.org>
2019-10-03 21:47:48 +0200

    Poprawa komendy pay

--- data/modules/cogs/Economy.py
+++ data/modules/cogs/Economy.py
@@ -12,24 +12,27 @@ class Economy(commands.Cog):
         server_id = server.id
         if server_id not in cr.server_economy:
             cr.server_economy[server_id] = cr.EcoMethods(server_id)
-        if amount < 0:
-            await ctx.send("Nie możesz płacić ujemną kwotą!")
-        elif amount == 0:
-            await ctx.send("Po co zawracasz głowę, skoro nie chcesz płacić!?")
+        if ctx.author.id == user.id:
+            await ctx.send("Nie ma sensu dawać sobie pieniądze, przecież już je masz!")
         else:
-            accounts = await cr.server_economy[server_id].check_accounts()
-            sender = str(ctx.author.id)
-            receiver = str(user.id)
-            account_sender = accounts[sender]
-            account_receiver = accounts[receiver]
-            if account_sender < amount:
-                await ctx.send("Nie masz wystarczających środków na koncie!")
+            if amount < 0:
+                await ctx.send("Nie możesz płacić ujemną kwotą!")
+            elif amount == 0:
+                await ctx.send("Po co zawracasz głowę, skoro nie chcesz płacić!?")
             else:
-                account_sender -= amount
-                account_receiver += amount
-                await cr.server_economy[server_id].money_transfer(sender, receiver, account_sender, account_receiver)
-                await ctx.send("Transakcja wykonana pomyślnie!\n"
-                               "Twój stan konta wynosi teraz {} $!".format(account_sender))
+                accounts = await cr.server_economy[server_id].check_accounts()
+                sender = str(ctx.author.id)
+                receiver = str(user.id)
+                account_sender = accounts[sender]
+                account_receiver = accounts[receiver]
+                if account_sender < amount:
+                    await ctx.send("Nie masz wystarczających środków na koncie!")
+                else:
+                    account_sender -= amount
+                    account_receiver += amount
+                    await cr.server_economy[server_id].money_transfer(sender, receiver, account_sender, account_receiver)
+                    await ctx.send("Transakcja wykonana pomyślnie!\n"
+                                   "Twój stan konta wynosi teraz {} $!".format(account_sender))
 
     @commands.command(aliases=["stan_konta"])
     async def money(self, ctx, user: discord.User=None):
