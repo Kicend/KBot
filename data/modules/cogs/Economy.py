bcfc6b6054949729ff7e8bae1c90aa1cf719ccd5
BuildTools <unconfigured@null.spigotmc.org>
2019-10-03 21:26:38 +0200

    Nowe komendy pay i money

--- data/modules/cogs/Economy.py
+++ data/modules/cogs/Economy.py
@@ -1,11 +1,43 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions
+from data.modules.utils import core as cr
 
 class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(aliases=["przelej"])
-    async def pay(self, user: discord.Member, amount):
-        return True
+    async def pay(self, ctx, user: discord.User, amount: int):
+        server = self.bot.get_guild(ctx.guild.id)
+        server_id = server.id
+        if server_id not in cr.server_economy:
+            cr.server_economy[server_id] = cr.EcoMethods(server_id)
+        accounts = await cr.server_economy[server_id].check_accounts()
+        sender = str(ctx.author.id)
+        receiver = str(user.id)
+        account_sender = accounts[sender]
+        account_receiver = accounts[receiver]
+        if account_sender < amount:
+            await ctx.send("Nie masz wystarczających środków na koncie!")
+        else:
+            account_sender -= amount
+            account_receiver += amount
+            await cr.server_economy[server_id].money_transfer(sender, receiver, account_sender, account_receiver)
+            await ctx.send("Transakcja wykonana pomyślnie!\n"
+                           "Twój stan konta wynosi teraz {} $!".format(account_sender))
+
+    @commands.command(aliases=["stan_konta"])
+    async def money(self, ctx, user: discord.User=None):
+        server = self.bot.get_guild(ctx.guild.id)
+        server_id = server.id
+        if server_id not in cr.server_economy:
+            cr.server_economy[server_id] = cr.EcoMethods(server_id)
+        if user is None:
+            money = await cr.server_economy[server_id].check_account(str(ctx.author.id))
+            await ctx.send("Posiadasz {} $ na koncie".format(money))
+        else:
+            money = await cr.server_economy[server_id].check_account(str(user.id))
+            await ctx.send("{} posiada {} $ na koncie".format(user.name, money))
+
+def setup(bot):
+    bot.add_cog(Economy(bot))
--- data/settings/bot_basic_parameters/config.py
+++ data/settings/bot_basic_parameters/config.py
@@ -3,11 +3,12 @@ import time
 # Podstawowe parametry bota
 TOKEN = 'NTcwMjg4NTM0MDIwMTYxNTM4.XL9qbA.z2aE8-wAdad78ox3Dt-N8oswTVA'
 commands_prefix = "!"
-wersja = "0.26-2"
+wersja = "0.27"
 boot_date = time.strftime("%H:%M %d.%m.%Y UTC")
 __cogs__ = [
     "data.modules.cogs.Entertainment",
     "data.modules.cogs.Administration",
     "data.modules.cogs.Music",
-    "data.modules.cogs.Utilities"
+    "data.modules.cogs.Utilities",
+    "data.modules.cogs.Economy"
     ]
