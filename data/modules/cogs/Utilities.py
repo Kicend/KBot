b75d748f86e25b89404dfc4d3bd4a1bb507d0a8b
BuildTools <unconfigured@null.spigotmc.org>
2019-10-07 18:31:59 +0200

    Zmiana prefixu w komendzie help

--- data/modules/cogs/Utilities.py
+++ data/modules/cogs/Utilities.py
@@ -14,11 +14,11 @@ class Utilities(commands.Cog):
 
     @commands.command(aliases=["pomocy"])
     async def help(self, ctx, los=1):
-        # server = self.bot.get_guild(ctx.guild.id)
-        # server_id = server.id
-        # if server_id not in cr.server_parameters:
-        #    cr.server_parameters[server_id] = cr.GuildParameters(server_id)
-        # prefix = await cr.server_parameters[server_id].prefix
+        server = self.bot.get_guild(ctx.guild.id)
+        server_id = server.id
+        if server_id not in cr.server_parameters:
+            cr.server_parameters[server_id] = cr.GuildParameters(server_id)
+        prefix = await cr.server_parameters[server_id].get_prefix()
 
         wersja = config.wersja
         decyzja = int(los)
@@ -28,18 +28,30 @@ class Utilities(commands.Cog):
             )
 
             embed.set_author(name="Sekcja pomocy bota KBot wersja {} (strona 1/4)".format(wersja))
-            embed.add_field(name="!wkrocz [nazwa kanału]", value="Wkracza z buta na czat głosowy", inline=False)
-            embed.add_field(name="!strumykuj <url>", value="Strumykuj z interneta pieśni", inline=False)
-            embed.add_field(name="!następna", value="Przewiń do kolejnej pieśni", inline=False)
-            embed.add_field(name="!adminnext", value="Pomiń pieśń jak król", inline=False)
-            embed.add_field(name="!pętla", value="Zapętlij pieśń", inline=False)
-            embed.add_field(name="!teraz", value="Wyświetl informacje o aktualnie granej pieśni", inline=False)
-            embed.add_field(name="!kolejka", value="Sprawdź zawartość kolejki", inline=False)
-            embed.add_field(name="!czyść_kolejke", value="Wyczyść kolejkę z niepotrzebnych pieśni", inline=False)
-            embed.add_field(name="!harmider <wartość (od 0 do 150)>", value="Zmienia głośność bota", inline=False)
-            embed.add_field(name="!pauzuj", value="Pauzuje graną pieśń", inline=False)
-            embed.add_field(name="!wznów", value="Wznawia wstrzymaną pieśń", inline=False)
-            embed.add_field(name="!wypad", value="Zatrzymuje bota i rozłącza go z czatem głosowym", inline=False)
+            embed.add_field(name="{}wkrocz [nazwa kanału]".format(prefix), value="Wkracza z buta na czat głosowy",
+                            inline=False)
+            embed.add_field(name="{}strumykuj <url>".format(prefix), value="Strumykuj z interneta pieśni",
+                            inline=False)
+            embed.add_field(name="{}następna".format(prefix), value="Przewiń do kolejnej pieśni",
+                            inline=False)
+            embed.add_field(name="{}adminnext".format(prefix), value="Pomiń pieśń jak król",
+                            inline=False)
+            embed.add_field(name="{}pętla".format(prefix), value="Zapętlij pieśń",
+                            inline=False)
+            embed.add_field(name="{}teraz".format(prefix), value="Wyświetl informacje o aktualnie granej pieśni",
+                            inline=False)
+            embed.add_field(name="{}kolejka".format(prefix), value="Sprawdź zawartość kolejki",
+                            inline=False)
+            embed.add_field(name="{}czyść_kolejke".format(prefix), value="Wyczyść kolejkę z niepotrzebnych pieśni",
+                            inline=False)
+            embed.add_field(name="{}harmider <wartość (od 0 do 150)>".format(prefix), value="Zmienia głośność bota",
+                            inline=False)
+            embed.add_field(name="{}pauzuj".format(prefix), value="Pauzuje graną pieśń",
+                            inline=False)
+            embed.add_field(name="{}wznów".format(prefix), value="Wznawia wstrzymaną pieśń",
+                            inline=False)
+            embed.add_field(name="{}wypad".format(prefix), value="Zatrzymuje bota i rozłącza go z czatem głosowym",
+                            inline=False)
 
             await ctx.send(embed=embed)
 
@@ -49,15 +61,22 @@ class Utilities(commands.Cog):
             )
 
             embed.set_author(name="Sekcja pomocy KBot wersja {} (strona 2/4)".format(wersja))
-            embed.add_field(name="!ping", value="Dowiedz się jak słabego mam neta", inline=False)
-            embed.add_field(name="!zaproszenie", value="We no zaproś na serwerek", inline=False)
-            embed.add_field(name="!autor", value="Wpiszta by się dowiedzieć więcej o Stwórcy tego dzieła", inline=False)
-            embed.add_field(name="!info_bot", value="Informacje o mnie", inline=False)
-            embed.add_field(name="!serwer", value="Informacje o serwerze", inline=False)
-            embed.add_field(name="!użytkownik <nick, @nick lub id>", value="Informacje o danym użytkowniku",
+            embed.add_field(name="{}ping".format(prefix), value="Dowiedz się jak słabego mam neta",
                             inline=False)
-            embed.add_field(name="!zmiana_prefixu <nowy prefix>", value="Zmiana prefixu bota", inline=False)
-            embed.add_field(name="!ustawienia", value="Panel Ustawień", inline=False)
+            embed.add_field(name="{}zaproszenie".format(prefix), value="We no zaproś na serwerek",
+                            inline=False)
+            embed.add_field(name="{}autor".format(prefix), value="Wpiszta by się dowiedzieć więcej o Stwórcy tego dzieła",
+                            inline=False)
+            embed.add_field(name="{}info_bot".format(prefix), value="Informacje o mnie",
+                            inline=False)
+            embed.add_field(name="{}serwer".format(prefix), value="Informacje o serwerze",
+                            inline=False)
+            embed.add_field(name="{}użytkownik <nick, @nick lub id>".format(prefix),
+                            value="Informacje o danym użytkowniku", inline=False)
+            embed.add_field(name="{}zmiana_prefixu <nowy prefix>".format(prefix), value="Zmiana prefixu bota",
+                            inline=False)
+            embed.add_field(name="{}ustawienia".format(prefix), value="Panel Ustawień",
+                            inline=False)
 
             await ctx.send(embed=embed)
 
@@ -67,13 +86,20 @@ class Utilities(commands.Cog):
             )
 
             embed.set_author(name="Sekcja pomocy KBot wersja {} (strona 3/4)".format(wersja))
-            embed.add_field(name="!kopnij", value="Kopnij w tyłek", inline=False)
-            embed.add_field(name="!ukarz", value="Ukarz delikwenta na tułaczkę", inline=False)
-            embed.add_field(name="!wybacz", value="Wybacz mu", inline=False)
-            embed.add_field(name="!skazańcy", value="Lista skazańców", inline=False)
-            embed.add_field(name="!czyść <liczba> [użytkownik]", value="Służba sprzątania czatu", inline=False)
-            embed.add_field(name="!dodaj_role <użytkownik> <rola>", value="Obdaruj użytkownika rolą", inline=False)
-            embed.add_field(name="!usuń_role <użytkownik> <rola>", value="Zabierz nikczemnikowi rolę", inline=False)
+            embed.add_field(name="{}kopnij".format(prefix), value="Kopnij w tyłek",
+                            inline=False)
+            embed.add_field(name="{}ukarz".format(prefix), value="Ukarz delikwenta na tułaczkę",
+                            inline=False)
+            embed.add_field(name="{}wybacz".format(prefix), value="Wybacz mu",
+                            inline=False)
+            embed.add_field(name="{}skazańcy".format(prefix), value="Lista skazańców",
+                            inline=False)
+            embed.add_field(name="{}czyść <liczba> [użytkownik]".format(prefix), value="Służba sprzątania czatu",
+                            inline=False)
+            embed.add_field(name="{}dodaj_role <użytkownik> <rola>".format(prefix), value="Obdaruj użytkownika rolą",
+                            inline=False)
+            embed.add_field(name="{}usuń_role <użytkownik> <rola>".format(prefix), value="Zabierz nikczemnikowi rolę",
+                            inline=False)
 
             await ctx.send(embed=embed)
 
@@ -83,13 +109,16 @@ class Utilities(commands.Cog):
             )
 
             embed.set_author(name="Sekcja pomocy KBot wersja {} (strona 4/4)".format(wersja))
-            embed.add_field(name="!kostka <ile ścian (minimum 4)>", value="Weźse wylosuj jakąś liczbunie szefuńciu",
+            embed.add_field(name="{}kostka <ile ścian (minimum 4)>".format(prefix),
+                            value="Weźse wylosuj jakąś liczbunie szefuńciu", inline=False)
+            embed.add_field(name="{}zapytaj <pytanie>".format(prefix), value="Zapytaj mnie o cokolwiek",
                             inline=False)
-            embed.add_field(name="!zapytaj <pytanie>", value="Zapytaj mnie o cokolwiek", inline=False)
-            embed.add_field(name="!pkn <papier, kamień lub nożyce>", value="Zagraj ze mną w papier, kamień i nożyce",
+            embed.add_field(name="{}pkn <papier, kamień lub nożyce>".format(prefix),
+                            value="Zagraj ze mną w papier, kamień i nożyce", inline=False)
+            embed.add_field(name="{}zgadywanka".format(prefix), value="Odgadnij liczbę",
                             inline=False)
-            embed.add_field(name="!zgadywanka", value="Odgadnij liczbę", inline=False)
-            embed.add_field(name="!moneta", value="Rzuć monetą", inline=False)
+            embed.add_field(name="{}moneta".format(prefix), value="Rzuć monetą",
+                            inline=False)
 
             await ctx.send(embed=embed)
 
@@ -177,7 +206,7 @@ class Utilities(commands.Cog):
         server_id = server.id
         if server_id not in cr.server_parameters:
             cr.server_parameters[server_id] = cr.GuildParameters(server_id)
-        server_config = await cr.server_parameters[server_id].config
+        server_config = await cr.server_parameters[server_id].check_config()
         if setting is None and switch is None:
             embed = discord.Embed(
                 colour=discord.Colour.blue()
