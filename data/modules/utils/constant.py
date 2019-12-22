704e13a1e5d91f44c9d244aa3c1770820d3f3908
Kicend <kicend@gmail.com>
2019-12-22 21:07:12 +0100

    Nowa stała

--- data/modules/utils/constant.py
+++ data/modules/utils/constant.py
@@ -3,3 +3,4 @@ GUILD_PARAMETERS = {"require_dj": "off", "QSP": "on", "autorole": None, "currenc
 DEFAULT_SERVER_PREFIX = "!"
 SERVERS_PREFIXES_FILE = "data/settings/servers_prefixes/prefixes.json"
 SERVERS_SETTINGS_FILES = "data/settings/servers_settings"
+SERVERS_ECO_DB = "data/eco_db"
