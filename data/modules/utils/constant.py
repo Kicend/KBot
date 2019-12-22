c17f3d887e2dbd763cf21ba6dbf1b7bb6af14f38
Kicend <kicend@gmail.com>
2019-12-22 15:44:51 +0100

    Plik ze stałymi w projekcie

---
+++ data/modules/utils/constant.py
@@ -0,0 +1,4 @@
+# Constants for KBot
+GUILD_PARAMETERS = {"require_dj": "off", "QSP": "on", "autorole": None, "currency_symbol": "$"}
+DEFAULT_SERVER_PREFIX = "!"
+SERVER_PREFIXES_FILE = "data/settings/servers_prefixes/prefixes.json"
